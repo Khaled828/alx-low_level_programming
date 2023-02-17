6fbe595d8f37f013223822127142a028ebb990f0
Khaled828 <khaledkandile269@gmail.com>
2023-02-17 06:03:00 -0800

    printing with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,11 @@
 
 /**
  * program that prints a line with puts function
+ * Return: 0(Success)
  */
 
-int main(void) {
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
