9da964a43dd714db2c36a42f8314115a6d97bffc
Khaled828 <khaledkandile269@gmail.com>
2023-02-18 11:13:33 -0800

    Help

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,11 +13,14 @@ int main(void)
 		for (j = i + 1; j < 10; j++)
 		{
 			putchar(i + '0');
-			putchar(',');
-			putchar(' ');
 			putchar(j + '0');
-			putchar('\n');
+			if (j != 89)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
+	putchar('\n');
 	return (0);
 }
